e1ce1e72da6f37da2cb1707c147104cca37cc23d
VIkas <vikas4cloud@gmail.com>
2020-10-13 21:18:54 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello batch15 team");
+     System.out.println("Hello batch16 team");
       System.out.println("Hello BJSHUB Batch 20 and training practice with nishi and jitendra an abcdefghi and some jobs for new build testing 2");
     }  
 }  
