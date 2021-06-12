1ba17c7b7a356df68710070649b1e970267e2d68
VIkas <vikas4cloud@gmail.com>
2021-06-12 13:33:14 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello batch16 team");
-      System.out.println("Hello BJSHUB Batch batch16 and batch17 people with suneel last ci for today and its easy fro ankita");
+      System.out.println("Hello BJSHUB Batch batch16 and batch17 people with suneel last ci for today and its easy fro ankita with ci poll scm opting enable ");
     }  
 }  
