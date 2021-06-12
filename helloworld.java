c211f6286b5933339ecf3c73b7ec2690c67ad9ba
Bharath R Shet <30365864+bc1213@users.noreply.github.com>
2021-06-12 17:08:22 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,7 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello batch16 team");
-      System.out.println("Hello BJSHUB Batch batch16 and batch17 people with suneel last ci for today and its easy fro ankita with ci poll scm opting enable 2nd time dev pushing the code ");
+     System.out.println("checking CI/CD");
+     System.out.println("Hello BJSHUB Batch batch16 and batch17 people with suneel last ci for today and its easy fro ankita with ci poll scm opting enable 2nd time dev pushing the code");
     }  
 }  
